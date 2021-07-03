98a56b1194ade6de17c1621b2d4b5d7f7731a4c1
Etienne Doumazane <74925515+E-Dmz@users.noreply.github.com>
2021-07-03 09:17:46 +0200

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -121,7 +121,7 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(<Game/>, document.getElementById("tictactoe_container"));
+ReactDOM.render(<Game/>, document.getElementById("root"));
 
 function calculateWinner(squares) {
   const lines = [
